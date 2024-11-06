e5ccd39882350502bfda1297cf304ccfd1407160
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-11-06 19:46:46 +0600

    added about section & scroll navbar bg change feture

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Hero from "./components/Hero/Hero"
-import Navbar from "./components/Navbar/Navbar"
-import Programs from "./components/Programs/Programs"
-import Title from "./components/Title/Title"
-
+import About from "./components/About/About";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Programs from "./components/Programs/Programs";
+import Title from "./components/Title/Title";
 
 function App() {
-
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="container">
-        <Title subTitle="Our Programs" title="What We Offer"/>
-      <Programs/>
+        <Title subTitle="Our Programs" title="What We Offer" />
+        <Programs />
+        <About/>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
