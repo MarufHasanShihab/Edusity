a65a70a82ab2f698746c534ec509b39f0e3eb1f7
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-11-07 18:56:28 +0600

    added footer and Scroll Feature (clicking to scroll to the section)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import "./Navbar.css"
 import logo from "../../assets/logo.png"
 import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
     const [sticky, setSticky]= useState(false);
@@ -14,12 +15,12 @@ const Navbar = () => {
         <nav className={`container ${sticky?"dark-nav":""}`}>
             <img src={logo} alt="nav_logo" className="logo" />
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className="btn">Contact us</button></li>
+                <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to="program" smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to="about" smooth={true} offset={-150} duration={500}>About us</Link> </li>
+                <li><Link to="campus" smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to="contact" smooth={true} offset={-260} duration={500}><button className="btn">Contact us</button></Link></li>
             </ul>
         </nav>
     );
